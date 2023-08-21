a3f754603e02862eadf314b51feaa2dc512d7075
Adil <adilbacker11@gmail.com>
2023-08-21 11:52:51 +0530

    first commit

---
+++ src/components/SideBar.js
@@ -0,0 +1,70 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "../styles/sidebar.css";
+import obs_logo from "../assets/logo.svg"
+import summary from "../assets/summary.svg";
+import cash from "../assets/cash.svg";
+// import summary from "../assets/summary.svg";
+import { useState } from 'react';
+
+
+const Icon = ({ name, text,imgUrl, isActive, handleClick }) => (
+    <div className={"side_link"} onClick={handleClick}>
+      {!isActive ? (
+        <div style={{'display': 'flex', 'justifyContent': 'flex-start', 'alignItems': 'center' }} >
+            <img src={imgUrl} alt="fund_logo" className={'icon_img'} />
+            <span className={"side_text"}>{text}</span>
+        </div>
+      ) : (
+        <div style={{'display': 'flex', 'justifyContent': 'flex-start', 'alignItems': 'center'}}>
+            <img src={imgUrl} alt="fund_logo" className={isActive !== name ? "icon_img" : "icon_img_color"} />
+            <span className={isActive && isActive === name ? "side_text_white" : "side_text"}>{text}</span>
+        </div>
+        
+      )}
+    </div>
+)
+
+export default function SideBar({ initial, acc_no, balance }) {
+
+  const router = useHistory();
+  const [isActive, setIsActive] = useState(initial);
+
+  return (
+    <div className={"sidebar_wrap"}>
+        <div className={"sidebar_cont"}>
+            <div className={"sidebar_bag"}>
+            <Link to="/">
+                <img className={"logo"} style={{'marginBottom': '30px'}} src={obs_logo} alt="logo" />
+            </Link>
+                <Icon key="dashboard" name="dashboard" imgUrl={summary}
+                isActive={isActive} text="Account Summary"
+                handleClick={() => {
+                    setIsActive("dashboard");
+                    router.push("/dashboard");
+                }}
+                />
+
+                <Icon key="transfer" name="transfer" imgUrl={cash}
+                isActive={isActive} text="Fund Transfer"
+                handleClick={() => {
+                    setIsActive("transfer");
+                    router.push({
+                      pathname: "/transfer",
+                      search: `${acc_no}?${balance}`
+                    });
+                }}
+                />
+
+                {/* <Icon key="beneficiaries" name="beneficiaries" imgUrl={profile}
+                isActive={isActive} text="Beneficiaries"
+                handleClick={() => {
+                    setIsActive("beneficiaries");
+                    router.push("/beneficiaries");
+                }}
+                /> */}
+            </div>
+        </div>
+    </div>
+  );
+}
---
+++ src/components/__test__/global.test.js
@@ -0,0 +1,51 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import Card from '../Card';
+import LogoutButton from '../LogoutButton';
+
+test('card render', () => {
+    render(<Card cardIcon='fa fa-money-check' cardLabel={"Total Spendings"}
+    cardInfo={`₹ 1000`} theme="dark" />)
+
+    const cardElement = screen.getByTestId('card-1');
+    expect(cardElement).toBeInTheDocument();
+    expect(cardElement).toHaveTextContent('Total Spendings');
+})
+
+test('logout button render', () => {
+    render(<LogoutButton theme="dark" />)
+
+    const sidebarElement = screen.getByTestId('logout-1');
+    expect(sidebarElement).toBeInTheDocument();
+    expect(sidebarElement).toHaveTextContent('Logout');
+})
+
+test('form validation', () => {
+    expect(true).toBe(true);
+})
+
+test('popup render', () => {
+    expect(true).toBe(true);
+})
+
+test('admin dashboard render', () => {
+    expect(true).toBe(true);
+})
+
+test('admin all_accounts api', async () => {
+    const res = await fetch(`http://localhost:8080/api/v1/accounts`)
+    const data = await res.json();
+
+    expect(data[0]["account_type"]).toEqual("savings");
+})
+
+test('navbar render', () => {
+    expect(true).toBe(true);
+})
+
+test('transaction render', () => {
+    expect(true).toBe(true);
+})
+
+test('account creation render', () => {
+    expect(true).toBe(true);
+})
